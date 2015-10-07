5317eb72f0132a4f9681eacfd4e162805583cb8f
Karsten Gresch <git-2015@by-doing.org>
2015-10-07 09:28:34 +0200

    Changed the countdown so it's working.

--- CounterAnimations/SingleCircleViewController.swift
+++ CounterAnimations/SingleCircleViewController.swift
@@ -9,64 +9,64 @@
 import UIKit
 
 class SingleCircleViewController: UIViewController {
-
+  
   @IBOutlet weak var singleCircle: SingleCircle?
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  var lastInterval = NSTimeInterval()
+  var timer = NSTimer()
+  var totalTime = NSTimeInterval()
+  let maxTime: Double = 12.0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  
   @IBAction func sliderChanged(sender: UISlider) {
     singleCircle?.arc = CGFloat(sender.value)
-    
-    
   }
-
+  
   @IBAction func startButtonPressed(sender: AnyObject) {
     print("Start button pressed")
- 
-    let totalTime = 12
     
-    for var second = 0; second<=totalTime-1; second++ {
-      print("second: \(second)")
-      let currentRatio: Float = Float(second/totalTime)
-      print("Current Ratio_ \(currentRatio)")
-      
-      let timer = NSTimer.new(every: 1.seconds)
-        {
-      
-      }
-      
-      timer.start()
-      self.singleCircle?.arc = CGFloat(second/totalTime)
-
-      
-        if second == totalTime-1
-        {
-          timer.invalidate()
-          break
-      }
-      
+    singleCircle?.arc = 0
+    
+    if !timer.valid {
+      let selector: Selector = "updateCounter"
+      timer = NSTimer.scheduledTimerWithTimeInterval(0.01,
+        target: self,
+        selector: selector,
+        userInfo: nil,
+        repeats: true)
+      lastInterval = NSDate.timeIntervalSinceReferenceDate()
+    } else {
+      singleCircle?.arc = 1.0
+      timer.invalidate()
     }
-      
-
+  }
+  
+  func updateCounter() {
+    
+    let now = NSDate.timeIntervalSinceReferenceDate()
+    totalTime += now - lastInterval
+    lastInterval = now
+    
+    if(totalTime < maxTime) {
+      singleCircle?.arc = CGFloat(totalTime / maxTime)
     }
+    else {
+      singleCircle?.arc = 1.0
+      totalTime = 0
+      timer.invalidate()
+    }
+    
+  }
+  
+  
+  
   
 }
