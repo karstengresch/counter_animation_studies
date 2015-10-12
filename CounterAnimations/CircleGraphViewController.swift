1ba390912cf44a2500e726a0d66ac8bb4a20ded3
Karsten Gresch <git-2015@by-doing.org>
2015-10-12 14:08:26 +0200

    Added timer to MakingAppPie example

--- CounterAnimations/CircleGraphViewController.swift
+++ CounterAnimations/CircleGraphViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class CircleGraphViewController: UIViewController {
+  
+  var lastInterval = NSTimeInterval()
+  var timer = NSTimer()
+  var totalTime = NSTimeInterval()
+  let maxTime: Double = 12.0
 
   @IBOutlet weak var percentLabel: UILabel?
 
@@ -40,6 +45,42 @@ class CircleGraphViewController: UIViewController {
     print("End arc value: \(circleGraphView?.endArc)")
     circleGraphView?.endArc = CGFloat(sender.value)
     percentLabel?.text = String(format: " %5.2f %%", sender.value * 100)
+    
+  }
+  @IBAction func startButtonPressed(sender: AnyObject) {
+    print("Start button pressed")
+    
+    circleGraphView?.endArc = 0
+    
+    if !timer.valid {
+      let selector: Selector = "updateCounter"
+      timer = NSTimer.scheduledTimerWithTimeInterval(0.01,
+        target: self,
+        selector: selector,
+        userInfo: nil,
+        repeats: true)
+      lastInterval = NSDate.timeIntervalSinceReferenceDate()
+    } else {
+      circleGraphView?.endArc = 1.0
+      timer.invalidate()
+    }
+  }
+  
+  func updateCounter() {
+    
+    let now = NSDate.timeIntervalSinceReferenceDate()
+    totalTime += now - lastInterval
+    lastInterval = now
+    
+    if(totalTime < maxTime) {
+      circleGraphView?.endArc = CGFloat(totalTime / maxTime)
+      percentLabel?.text = String(format: " %5.2f %%", totalTime / maxTime * 100)
+    }
+    else {
+      circleGraphView?.endArc = 1.0
+      totalTime = 0
+      timer.invalidate()
+    }
     
   }
 
