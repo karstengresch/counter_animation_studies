a5b67d9a74ce374dc681bd2c0b792e6b84f04b40
Karsten Gresch <git-2015@by-doing.org>
2017-09-10 22:00:28 +0200

    Fiddling around /w basic header elements

--- MaterialCosmicMindStudies/MaterialCosmicMindStudies/AppDelegate.swift
+++ MaterialCosmicMindStudies/MaterialCosmicMindStudies/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Material
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    window = UIWindow(frame: Screen.bounds)
+    
+    // window!.rootViewController = ToolbarController(rootViewController: ViewController()) => of course fully empty
+    window!.makeKeyAndVisible()
+    
+    
     return true
   }
 
